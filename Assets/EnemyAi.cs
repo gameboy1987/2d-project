2b9482f71f91e314ce220841706dd00ba00585bb
MUKILTEO\1725476 <1725476@mukilteo.wednet.edu>
2025-11-05 13:25:51 -0800

    w

--- Assets/EnemyAi.cs
+++ Assets/EnemyAi.cs
@@ -17,6 +17,8 @@ public class EnemyAi : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
